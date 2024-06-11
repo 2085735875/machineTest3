674ade8f99536e886b1fc61f990dbca162a5b8d5
2085735875 <rupeshdesale79@gmail.com>
2024-06-11 13:23:10 +0530

    work on TD form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'machineTest3';
+
+  @ViewChild("signUpForm") signUpForm !: NgForm
+
+  onSignUp(signUpForm : NgForm){
+    if(signUpForm.valid){
+      console.log(signUpForm)
+    console.log(signUpForm.value)
+    signUpForm.resetForm()
+    }
+  }
 }
